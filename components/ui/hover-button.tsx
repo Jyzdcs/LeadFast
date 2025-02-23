1f0d99d4f1c1fe5219f3d16bc7144e0aacba1a7e
Jyzdcs <ky.claudant@gmail.com>
2025-02-24 00:52:23 +0100

    removed error of build

--- components/ui/hover-button.tsx
+++ components/ui/hover-button.tsx
@@ -104,9 +104,9 @@ const HoverButton = React.forwardRef<HTMLButtonElement, HoverButtonProps>(
         onPointerLeave={handlePointerLeave}
         {...props}
         style={{
-          "--circle-start": "var(--tw-gradient-from, #a0d9f8)",
-          "--circle-end": "var(--tw-gradient-to, #3a5bbf)",
-        }}
+          ["--circle-start" as string]: "var(--tw-gradient-from, #a0d9f8)",
+          ["--circle-end" as string]: "var(--tw-gradient-to, #3a5bbf)",
+        } as React.CSSProperties}
       >
         {circles.map(({ id, x, y, color, fadeState }) => (
           <div
